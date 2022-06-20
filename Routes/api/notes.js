8cc63a7b4a553344ca69c8ab6a04ec94f99e0a2e
lacey gibler <maxsonlacey@gmail.com>
2022-06-19 23:24:11 -0400

    server

--- Routes/api/notes.js
+++ Routes/api/notes.js
@@ -1,11 +1,32 @@
 const notes = require('express').Router();
-const{readAndWriteFile, readFromFile}= require('../db/fsUtils');
+const{readAndWriteFile, readFromFile, getNoteIndex, deleteNote}= require('../../db/fsUtils');
+const {v4: uuidv4}= require('uuid');
 
-notes.get('/notes',(req, res)=> {
-    readFromFile('../../db.json')
+notes.get('/',(req, res)=> {
+    readFromFile('./db/db.json')
     .then((data) =>{
-        res,json(JSON.parse(data))
+    console.log(data);
+    res.json(JSON.parse(data))
     })
+})
+notes.get('/',(req, res) => {
+    getNoteIndex( './db/db.json')
+    .then((data) => {
+        console.log(data);
+        res.json(JSON.parse(data))
+    })
+})
+notes.post('/', (req,res)=>{
+   req.body.id=uuidv4();
+    console.log(req.body);
+    readAndWriteFile(req.body,'./db/db.json')
+    res.send(req.body)
+})
+
+notes.delete('/:id',(req, res) => {
+    console.log(req.params)
+    deleteNote('../db/db.json', req.params.id)
+    res.send(req.body)
 })
 
 module.exports= notes
